5ab54eeabd5abdb18555546cd0319b5a955648e2
LironTo <lirontom@post.bgu.ac.il>
2025-01-19 04:43:09 +0200

    client finishes (with the lords help) 2

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ TCP_PORT = 12345
 UDP_PORT = 13117
 MAGIC_COOKIE = 0xabcddcba
 MESSAGE_TYPE_REQUEST = 0x3
+MESSAGE_TYPE_OFFER = 0x2
 
 
 def listen_for_offer_udp(byte_size, tcp_connections, udp_connections):
@@ -16,16 +17,16 @@ def listen_for_offer_udp(byte_size, tcp_connections, udp_connections):
     print("Client started, listening for offer requests...")
     data, addr = udp_socket.recvfrom(1024)
     print("Received offer from: %s" % addr[0])
-    data_converted = data
-    if(data_converted[0:4] == MAGIC_COOKIE.to_bytes(4, byteorder="big") and data_converted[4:5] == (0x2).to_bytes(1, byteorder="big")):
-        print("Bad payload received")
+    data_converted = struct.unpack('IbHH', data)
+    if(data_converted[0] == MAGIC_COOKIE and data_converted[1] == MESSAGE_TYPE_OFFER):
+        print("Offer is valid, sending back request")
         threads = []
         counter = 1
         for i in range(tcp_connections):
-            threads.append(threading.Thread(tcp_connection(byte_size, addr[0], data[7:9].decode('ascii'),counter)))
+            threads.append(threading.Thread(tcp_connection(byte_size, addr[0], data_converted[3], counter)))
             counter += 1
         for i in range(udp_connections):
-            threads.append(threading.Thread(udp_connection(byte_size, addr[0], data[5:7].decode('ascii'),counter)))
+            threads.append(threading.Thread(udp_connection(byte_size, addr[0], data_converted[2], counter)))
             counter += 1
 
         for thread in threads:
@@ -42,17 +43,26 @@ def listen_for_offer_udp(byte_size, tcp_connections, udp_connections):
 
 def tcp_connection(byte_size, address, port, id):
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect((address, int(port, 16)))
+
+    tcp_socket.connect((address, port))
     start_time = time.time()
-    tcp_request_packet = struct.pack('IbH', MAGIC_COOKIE, MESSAGE_TYPE_REQUEST, hex(byte_size))
+    tcp_request_packet = struct.pack('QIB', byte_size, MAGIC_COOKIE, MESSAGE_TYPE_REQUEST)
     tcp_socket.send(tcp_request_packet)
-    data, addr = tcp_socket.recv(byte_size)
+    data = tcp_socket.recv(21)
+    print(f"Received message: {data}")
+    print(f"Data length: {len(data)}")
     end_time = time.time()
-    data_converted = struct.unpack('IbH', data)
+    print("RECIEVIED!")
+    data_converted = struct.unpack('QQIB', data)
+    print("Received message!!!: " + str(data_converted))
     if(data_converted[0] != MAGIC_COOKIE or data_converted[1] != 0x4):
         print("Bad payload received in UDP connection " + str(id)+"#")
         return
     print("TCP transfer #" + str(id) + " finished, total time: " + str(round(end_time - start_time, 2)) + " seconds, total speed: " + str(round(byte_size*8/(end_time - start_time), 2)) + " bits/second")
+    tcp_socket.close()
+
+    print("Connection failed")
+    #print(f"Error: {e}")
     tcp_socket.close()
 
 def udp_connection(byte_size, address, port, id):
