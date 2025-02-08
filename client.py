4b3d07ec0990b19681e3fbea26dbda8230afd9c9
Ohad Shapurkar <shapurka@post.bgu.ac.il>
2025-02-08 16:30:36 +0200

    almost done

--- client.py
+++ client.py
@@ -3,8 +3,7 @@ import struct
 import threading
 import time
 
-TCP_PORT = 12345
-UDP_PORT = 13117
+BROADCAST_PORT = 13117
 MAGIC_COOKIE = 0xabcddcba
 MESSAGE_TYPE_PAYLOAD = 0x4
 MESSAGE_TYPE_REQUEST = 0x3
@@ -14,21 +13,24 @@ MESSAGE_TYPE_OFFER = 0x2
 def listen_for_offer_udp(byte_size, tcp_connections, udp_connections):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_socket.bind(("", 13117))
+    udp_socket.bind(("", BROADCAST_PORT))
     print("Client started, listening for offer requests...")
     data, addr = udp_socket.recvfrom(9)
     print("Received offer from: %s" % addr[0])
-    data_converted = struct.unpack('IHHB', data)
-    if(data_converted[0] == MAGIC_COOKIE and data_converted[3] == MESSAGE_TYPE_OFFER):
+    data_converted = struct.unpack('!IBHH', data)
+    data_converted = list(data_converted)
+    if(data_converted[0] == MAGIC_COOKIE and data_converted[1] == MESSAGE_TYPE_OFFER):
+        udp = data_converted[2]
+        tcp = data_converted[3]
         print("Offer received, connecting to server")
         tcp_threads = []
         udp_threads = []
         for i in range(tcp_connections):
-            tcp_thread = threading.Thread(target=tcp_connection, args=(byte_size//tcp_connections, addr[0], TCP_PORT, i))
+            tcp_thread = threading.Thread(target=tcp_connection, args=(byte_size//tcp_connections, addr[0], tcp, i))
             tcp_threads.append(tcp_thread)
             tcp_thread.start()
         for i in range(udp_connections):
-            udp_thread = threading.Thread(target=udp_connection, args=(byte_size//udp_connections, addr[0], 13355, i))
+            udp_thread = threading.Thread(target=udp_connection, args=(byte_size//udp_connections, addr[0], udp, i))
             udp_threads.append(udp_thread)
             udp_thread.start()
         for thread in tcp_threads:
@@ -75,35 +77,50 @@ def tcp_connection(byte_size, address, port, id):
 
 def udp_connection(byte_size, address, port, id):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    packet = str(byte_size)
-    udp_socket.sendto(packet.encode(), (address, port))
+    
+    HEADER_SIZE = struct.calcsize('!IBQQ')  # Calculate header size
+    payload_size = byte_size - HEADER_SIZE  # Only the actual payload
+    
+    # Send request packet
+    udp_request_packet = struct.pack('!IBQ', MAGIC_COOKIE, MESSAGE_TYPE_REQUEST, payload_size)
+    udp_socket.sendto(udp_request_packet, (address, port))
+    
     start_time = time.time()
     udp_socket.settimeout(10)
-    #udp_request_packet = struct.pack('IbH', MAGIC_COOKIE, MESSAGE_TYPE_REQUEST, hex(byte_size))
-    #udp_socket.send(udp_request_packet)
+    
     data_count = 0
-    while data_count < byte_size:
+    
+    while data_count < payload_size:
         try:
-            data = udp_socket.recv(1024)
+            data = udp_socket.recv(8192)
             if not data:
                 break
-            data_count += len(data)
-            if not data:
-                raise Exception("Connection closed by server")
+            
+            if len(data) < HEADER_SIZE:
+                print(f"Received incomplete packet: {len(data)} bytes")
+                continue  # Skip processing if data is too small
+
+            payload_data = data[HEADER_SIZE:]  # Exclude header
+            data_count += len(payload_data)  # Count only payload bytes
+
         except Exception as e:
-            print("An error occurred in UDP connection " + str(id) + "#, error: " + str(e))
+            print(f"An error occurred in UDP connection {id}#, error: {e}")
             break
+
     end_time = time.time()
-    print("Data count: " + str(data_count))
-    print("Byte size: " + str(byte_size))
-    # if data_count == byte_size:
-    #     data_conversion = struct.unpack('IHHB', data)
-    #     if(data_conversion[0] == MAGIC_COOKIE and data_conversion[3] == MESSAGE_TYPE_PAYLOAD):
-    #         print("Data received")
-    #     else:
-    #         print("Data not received")
-    print("UDP transfer #" + str(id) + " finished, total time: " + str(round(end_time - start_time, 2)) + " seconds, total speed: " + str(round(byte_size*8/(end_time - start_time), 2)) + " bits/second" \
-        + "total percentage of data recevied: " + str(round(data_count/byte_size*100, 2)) + "%")
+    
+    print(f"Data count (payload only): {data_count}")
+    print(f"Expected payload size: {payload_size}")
+    
+    if data_count == payload_size:
+        print("Data received successfully")
+    else:
+        print("Data not fully received")
+    
+    print(f"UDP transfer #{id} finished, total time: {round(end_time - start_time, 2)} seconds, "
+          f"total speed: {round(payload_size * 8 / (end_time - start_time), 2)} bits/second, "
+          f"total percentage of data received: {round(data_count / payload_size * 100, 2)}%")
+    
     udp_socket.close()
 
 
@@ -111,8 +128,9 @@ def udp_connection(byte_size, address, port, id):
 
 
 
+
 def main():
-    byte_size = 16#input("Enter file size (in Bytes):")
+    byte_size = 999999#input("Enter file size (in Bytes):")
     tcp_connections = 6#input("Enter number of TCP connections:")
     udp_connections = 6#input("Enter number of UDP connections:")
 
--- server.py
+++ server.py
@@ -5,8 +5,12 @@ import threading
 import random
 import math
 
+
+BROADCAST_PORT = 13117
 TCP_PORT = 12345
-UDP_PORT = 13117
+UDP_PORT = 13355
+
+
 magic_cookie = 0xabcddcba
 message_type = 0x2
 request_type = 0x3
@@ -34,16 +38,16 @@ def udp_payload(addr, size):
     total = 0
     while total < size:
         if size - total < packet_size:
-            # header = struct.pack("ibH", magic_cookie, hex(payload_type))
-            # packet = header + b'a' * (size - total)
-            # udp_socket.sendto(packet, addr)
-            udp_socket.sendto(b'a' * (size - total), addr)
+            header = struct.pack("!IBQQ", magic_cookie, payload_type, size, size - total)
+            packet = header + b'a' * (size - total)
+            udp_socket.sendto(packet, addr)
+            # udp_socket.sendto(b'a' * (size - total), addr)
             total = size
         else:
-            # header = struct.pack("ibH", magic_cookie, hex(payload_type))
-            # packet = header + b'a' * packet_size
-            # udp_socket.sendto(packet, addr)
-            udp_socket.sendto(b'a' * packet_size, addr)
+            header = struct.pack("!IBQQ", magic_cookie, payload_type, size, packet_size)
+            packet = header + b'a' * packet_size
+            udp_socket.sendto(packet, addr)
+            # udp_socket.sendto(b'a' * packet_size, addr)
             total += packet_size
     udp_socket.close()
 
@@ -54,30 +58,40 @@ def wait_for_clients_tcp():
     tcp_server.bind(address)
     tcp_server.listen()
     thread_list = []
-    while True:
-        conn, addr = tcp_server.accept()
-        print("Connection from", addr)
-        thread = threading.Thread(target=tcp_payload, args=(conn, addr))
-        thread_list.append(thread)
-        thread.start()
+    try:
+        while True:
+            conn, addr = tcp_server.accept()
+            print("Connection from", addr)
+            thread = threading.Thread(target=tcp_payload, args=(conn, addr))
+            thread_list.append(thread)
+            thread.start()
+    except KeyboardInterrupt:
         for thread in thread_list:
             thread.join()
 
 def wait_for_clients_udp():
-    address = ('', 13355)
+    address = ('', UDP_PORT)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)   
     udp_socket.bind(address)
     thread_list = []
-    while True:
-        data, addr = udp_socket.recvfrom(1024)
-        print("Connection from", addr)
-        #header = struct.unpack("IHHB", data)
-        #if(header[0] == magic_cookie and header[3] == request_type):
-        size = int(data.decode(),10)
-        #print("size = " + str(size))
-        thread = threading.Thread(target=udp_payload, args=(addr, size))
-        thread_list.append(thread)
-        thread.start()
+    try:
+        while True:
+            data, addr = udp_socket.recvfrom(1024)
+            print("Connection from", addr)
+            header = struct.unpack("!IBQ", data)
+            data_list = list(header)
+            for i in data_list:
+                print(i)
+            if data_list[0] != magic_cookie or data_list[1] != request_type:
+                print("Invalid request")
+                continue
+            size = data_list[2]
+            print("size = " + str(size))
+            # size = int(data.decode(),10)
+            thread = threading.Thread(target=udp_payload, args=(addr, size))
+            thread_list.append(thread)
+            thread.start()
+    except KeyboardInterrupt:
         for thread in thread_list:
             thread.join()
         #else:
@@ -87,9 +101,9 @@ def broadcast():
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     server.settimeout(0.2)
-    message = struct.pack("IHHB", magic_cookie, UDP_PORT, TCP_PORT, message_type)
+    message = struct.pack("!IBHH", magic_cookie, message_type, UDP_PORT, TCP_PORT)
     while True:
-        server.sendto(message, ('<broadcast>', UDP_PORT))
+        server.sendto(message, ('<broadcast>', BROADCAST_PORT))
         time.sleep(1)
 
 def start_server():
